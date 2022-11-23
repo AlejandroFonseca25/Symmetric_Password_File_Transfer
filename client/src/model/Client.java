dfaad13ea9f76460f2007d37cff0015e05f0f3a8
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2022-11-23 14:29:07 -0500

    Added DiffieHellman classes

--- client/src/model/Client.java
+++ client/src/model/Client.java
@@ -3,6 +3,15 @@ package model;
 public class Client {
 	
 	public static void main (String[] args) {
-		
+		try {
+			//Connection values
+			int port = 8088;
+			String serverIp = "localhost";
+			
+			//Diffie Hellman 
+			
+		} catch (Exception e){
+			
+		}
 	}
 }
